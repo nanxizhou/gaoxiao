62dc6dbcc30cc941829f61e7f3c2efd6560472c6
nanxizhou <103089325+nanxizhou@users.noreply.github.com>
2022-04-11 10:25:56 +0800

    Changes

--- Emily/1.py
+++ Emily/1.py
@@ -32,9 +32,9 @@ with open('./04_月考勤表.xlsx', 'rb') as f:
     file_data = f.read()
 
 # 设置内容类型为附件
-#attachment = MIMEText(file_data, 'base64', 'utf-8')
+attachment = MIMEText(file_data, 'base64', 'utf-8')
 # 设置附件标题以及文件类型
-#attachment.add_header('Content-Disposition', 'attachment', filename='04_月考勤表.xlsx')
+attachment.add_header('Content-Disposition', 'attachment', filename='04_月考勤表.xlsx')
 
 # 创建复合邮件对象
 msg = MIMEMultipart()
